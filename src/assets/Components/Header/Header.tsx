afb8f8e01205ccf432ed26420e81b6d4295a60a4
Maharram Alverdiyev <maharramalverdiyev890@gmail.com>
2024-11-20 04:09:50 +0400

    header-done

--- src/assets/Components/Header/Header.tsx
+++ src/assets/Components/Header/Header.tsx
@@ -1,32 +1,87 @@
 import '../Header/header.css'
-import { logo } from "../../Images/XARİBULBUL.png";
+import logo from "../../Images/logo.png"
+import { CiFacebook } from "react-icons/ci";
+import { SlSocialInstagram } from "react-icons/sl";
+import { LiaTelegram } from "react-icons/lia";
+import { FaTiktok } from "react-icons/fa";
+import { VscAccount } from "react-icons/vsc";
+import { SlBasket } from "react-icons/sl";
+import { FaSearch } from "react-icons/fa";
+import { MdKeyboardArrowDown } from "react-icons/md";
 
 export const Header = () => {
     return (
-        <>
-        <header className="container">
-            <div className="logo"><a href="http://127.0.0.1:5500/index.html">
-                <div className="img">
-                    <img src={logo} alt="" />
-                </div>
-                <div><h3>QARABAGH <br />PERFUMERY</h3></div>
-            </a>
+        <header>
+            <div className="up-header">
+                <a href=""><CiFacebook /></a>
+                <a href=""><SlSocialInstagram /></a>
+                <a href=""><LiaTelegram /></a>
+                <a href=""><FaTiktok /></a>
             </div>
-            <nav>
-                <ul>
-                    <li><a href="" >ANA SƏHİFƏ</a></li>
-                    <li><a href="" >BRENDLƏR</a></li>
-                    <li><a href="" >HAQQIMIZDA</a></li>
-                    <li><a href="" >ƏLAQƏ</a></li>
-                </ul>
-            </nav>
-            <div className="icons">
-                <input placeholder="Öz ətrini axtar!" />
-                <a href="search"><i className="fa-solid fa-magnifying-glass"></i></a>
-                <a href="whatsapp"><i className="fa-regular fa-heart"></i></a>
-                <a href="https://www.instagram.com/qarabaghperfume_az/"><i className="fa-solid fa-bag-shopping"></i></a>
+            <div className="down-header">
+                <div className="logo">
+                    <img src={logo} alt="" />
+                    <h1>QARABAGH <br /> PERFUMERY</h1>
+                </div>
+                <div className="search-bar">
+                    <input type="text" placeholder='Öz ətirini axtar' />
+                    <div className="search-button"> <FaSearch /></div>
+                </div>
+                <div className="login-basket">
+                    <div className="login">
+                        <VscAccount />
+                        <p>Login</p>
+                    </div>
+                    <div className="basket">
+                        <SlBasket />
+                        <p>Səbət</p>
+                    </div>
+                </div>
+            </div>
+            <div className="nav-bar-header">
+                <nav>
+                    <ul>
+                        <li>Haqqımızda<MdKeyboardArrowDown /></li>
+                        <li>Bloq<MdKeyboardArrowDown /></li>
+                        <li>Kampaniyalar<MdKeyboardArrowDown /></li>
+                        <li>Dəstək<MdKeyboardArrowDown /></li>
+                    </ul>
+                </nav>
+            </div>
+            <div className="products-navbar">
+                <p>Brendlər: </p>
+                <nav>
+                    <ul>
+                        <li>A</li>
+                        <li>B</li>
+                        <li>C</li>
+                        <li>D</li>
+                        <li>E</li>
+                        <li>F</li>
+                        <li>G</li>
+                        <li>H</li>
+                        <li>I</li>
+                        <li>J</li>
+                        <li>K</li>
+                        <li>L</li>
+                        <li>M</li>
+                        <li>N</li>
+                        <li>O</li>
+                        <li>P</li>
+                        <li>Q</li>
+                        <li>R</li>
+                        <li>S</li>
+                        <li>T</li>
+                        <li>U</li>
+                        <li>V</li>
+                        <li>W</li>
+                        <li>X</li>
+                        <li>Y</li>
+                        <li>Z</li>
+                    </ul>
+                </nav>
+                <p>Bütün brendlər</p>
             </div>
         </header>
-        </>
     )
 }
--- src/assets/Components/SectionOne/SectionOne.tsx
+++ src/assets/Components/SectionOne/SectionOne.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import img1 from '../../Images/IMG_8838.jpg';
+import img2 from '../../Images/IMG_4448.jpg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './SectionOne.css';
 
 export const SectionOne = () => {
   return (
-    <div>Salam</div>
-  )
-}
-
+    <div className='carousel'>
+      <Carousel>
+        <Carousel.Item interval={500}>
+          <img className="carousel-image" src={img1} alt="First slide" />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={500}>
+          <img className="carousel-image" src={img2} alt="Second slide" />
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+};
