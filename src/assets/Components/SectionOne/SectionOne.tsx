98c10d9b44dd34083a27f0ce1a68d1a553c19f48
Maharram Alverdiyev <maharramalverdiyev890@gmail.com>
2024-11-20 15:32:30 +0400

    secOne-done

--- src/assets/Components/SectionOne/SectionOne.tsx
+++ src/assets/Components/SectionOne/SectionOne.tsx
@@ -1,29 +1,37 @@
-import React from 'react';
+
 import Carousel from 'react-bootstrap/Carousel';
-import img1 from '../../Images/IMG_8838.jpg';
+import img1 from '../../Images/image0 (1).jpeg';
 import img2 from '../../Images/IMG_4448.jpg';
+import img3 from '../../Images/IMG_3633.jpg';
+import img4 from '../../Images/IMG_8007.jpg'
+import gif from '../../Images/WhatsAppVideo2024-08-06at05.46.04-ezgif.com-video-to-gif-converter.gif'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './SectionOne.css';
 
 export const SectionOne = () => {
   return (
-    <div className='carousel'>
-      <Carousel>
-        <Carousel.Item interval={500}>
-          <img className="carousel-image" src={img1} alt="First slide" />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item interval={500}>
-          <img className="carousel-image" src={img2} alt="Second slide" />
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
+    <div className='secOne'>
+      <div className="secOne-center">
+        <div className='carousel'>
+          <Carousel className='carousel-slide'>
+            <Carousel.Item interval={1000}>
+              <img className="carousel-image" src={img1} alt="First slide" />
+            </Carousel.Item>
+            <Carousel.Item interval={1000}>
+              <img className="carousel-image" src={img2} alt="Second slide" />
+            </Carousel.Item>
+            <Carousel.Item interval={1000}>
+              <img className="carousel-image" src={img3} alt="Second slide" />
+            </Carousel.Item>
+            <Carousel.Item interval={1000}>
+              <img className="carousel-image" src={img4} alt="Second slide" />
+            </Carousel.Item>
+          </Carousel>
+        </div>
+        <div className="videomp4">
+          <img src={gif} alt="" />
+        </div>
+      </div>
     </div>
   );
 };
